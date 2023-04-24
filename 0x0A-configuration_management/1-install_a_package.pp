d27fb5a3999b8574cefc7b80296a45098d5882e2
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-04-24 03:50:42 +0100

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install flask from pip3
 
 package { 'flask==2.1.0':
-  path	=> ['/usr/bin', '/bin', '/usr/sbin']
+  path     => ['/usr/bin', '/bin', '/usr/sbin']
   ensure   => installed,
   name     => 'flask',
   provider => 'pip3'
