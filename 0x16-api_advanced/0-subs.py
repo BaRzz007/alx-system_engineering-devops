2d2e02a6e8e4ef62b2566f696d1d3a51777624b5
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-06-13 03:44:23 +0100

    number ob subs of subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries reddit api and returns number of subscribers
+    for a given subreddit"""
+    try:
+        resp = requests.get(url=f'https://www.reddit.com/r/{subreddit}/about.json',
+                            headers={"User-Agent": "myPyscript"},
+                            allow_redirects=False)
+
+        data = resp.json()
+        return data['data']['subscribers']
+    except Exception:
+        return 0
