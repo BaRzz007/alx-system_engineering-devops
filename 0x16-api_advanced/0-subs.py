02e300e95556f71f4275631d206d63bfa6ff2a48
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-06-13 03:52:03 +0100

    new line at end of file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,10 @@ def number_of_subscribers(subreddit):
     """Queries reddit api and returns number of subscribers
     for a given subreddit"""
     try:
-        resp = requests.get(url=f'https://www.reddit.com/r/{subreddit}/about.json',
-                            headers={"User-Agent": "myPyscript"},
-                            allow_redirects=False)
+        resp = requests.get(
+            url=f'https://www.reddit.com/r/{subreddit}/about.json',
+            headers={"User-Agent": "myPyscript"},
+            allow_redirects=False)
 
         data = resp.json()
         return data['data']['subscribers']
